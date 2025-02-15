243d08ec72e6843063ea74a0d3e5318f9851746d
kishor <rizwanikhan92@gmail.com>
2025-02-15 11:52:28 +0530

    updated

--- utils/generateLink.js
+++ utils/generateLink.js
@@ -1,5 +1,13 @@
+// const generateCustomLink = (id) => {
+//     const BASE_URL = process.env.BASE_URL || `http://localhost:${process.env.PORT || 8080}`;
+//     return `${BASE_URL}/api/template2/details/${id}`;  // Use path parameter format
+// };
+
+// module.exports = generateCustomLink;
+
+
 const generateCustomLink = (id) => {
-    const BASE_URL = process.env.BASE_URL || `http://localhost:${process.env.PORT || 8080}`;
+    const BASE_URL = process.env.BASE_URL || "https://call2connectbackend.onrender.com";
     return `${BASE_URL}/api/template2/details/${id}`;  // Use path parameter format
 };
 
